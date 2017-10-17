fce17bade5ace7a1da5d685a032f0d917bd85f27
FIC <a.bmoraes@outlook.com>
2017-10-17 20:43:14 -0200

    Array e Split

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ExemploArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = "amanda.moraes.botelho";
+            string[] strArray;
+
+            strArray = str.Split(".");
+
+            foreach(string palavra in strArray){
+                Console.WriteLine(palavra);
+            }
+        }
+    }
+}
